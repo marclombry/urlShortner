a13ad9c64856875e6deffb59f576c1820269dd60
Alex Lombry <alex.lombry@gmail.com>
2016-08-27 16:26:49 +0200

    Update indentation

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,88 +11,98 @@
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
-  <body>
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
+</head>
+<body>
+<!-- Fixed navbar -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Bootstrap theme</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Bootstrap theme</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                       aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li class="dropdown-header">Nav header</li>
+                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
         </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-      <div class="container theme-showcase" role="main">
-      <!-- Main jumbotron for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1>Theme example</h1>
-        <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
-      </div>
-    <div class="container">
-      <h2>URL SHORTNER</h2>
-      <form method="POST" action="traitement.php">
-        <div class="form-group">
-          <label for="url">URL:</label>
-          <input type="url" class="form-control" id="url" placeholder="Enter un url">
-        </div>
-        </div>
-        <button type="submit" class="btn btn-default">Envoyer</button>
-      </form>
     </div>
+</nav>
+
+<div class="container theme-showcase" role="main">
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <h1>Theme example</h1>
+        <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point
+            to create something more unique by building on or modifying it.</p>
+    </div>
+
     <div class="container">
-        <?php
-            $bdd = new PDO('mysql:host=localhost;port=3307;dbname=urlbase', 'root', '');
-            $requete = $bdd->query("SELECT * FROM urls");
-        ?>       
-      <table class="table">
+        <h2>URL SHORTNER</h2>
+        <form method="POST" action="traitement.php">
+            <div class="form-group">
+                <label for="url">URL:</label>
+                <input type="url" class="form-control" id="url" placeholder="Enter un url">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-default">Envoyer</button>
+            </div>
+        </form>
+    </div>
+
+</div>
+
+<div class="container">
+    <?php
+    $bdd = new PDO('mysql:host=localhost;port=3307;dbname=urlbase', 'root', '');
+    $requete = $bdd->query("SELECT * FROM urls");
+    ?>
+    <table class="table">
         <thead>
-          <tr>
+        <tr>
             <th>Vrai url</th>
             <th>url generé</th>
             <th>nombre de click</th>
-          </tr>
+        </tr>
         </thead>
         <tbody>
-        <?php foreach($requete->fetchAll() as $resultat) { ?>
-          <tr class="success">
-            <td><?php echo $resultat['url'] ?></td>
-            <td><?php echo $resultat['url_genere'] ?></td>
-            <td><?php echo $resultat['clicks'] ?></td>
-          </tr> 
-          <?php } ?>
+        <?php foreach ($requete->fetchAll() as $resultat) { ?>
+            <tr class="success">
+                <td><?php echo $resultat['url'] ?></td>
+                <td><?php echo $resultat['url_genere'] ?></td>
+                <td><?php echo $resultat['clicks'] ?></td>
+            </tr>
+        <?php } ?>
         </tbody>
-      </table>
-    </div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+    </table>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
 </html>
